a1c615374d6f3ba1f5515d41e1cd95c525ac7ae5
M4gic1540 <t.gonzalezb24@gmail.com>
2026-04-04 17:44:08 -0300

    fix: eliminar data-ingestion-service duplicado dentro de authservice

--- authservice/data-ingestion-service/src/main/java/com/main/dataingestion/repository/SyncRunRepository.java
+++
@@ -1,7 +0,0 @@
-package com.main.dataingestion.repository;
-
-import com.main.dataingestion.domain.SyncRun;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SyncRunRepository extends JpaRepository<SyncRun, Long> {
-}
